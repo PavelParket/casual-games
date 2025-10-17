d906a753aec8485b39b14250bc8c309a7d182a61
Pavel Stankevich <pavelstankevich04@gmail.com>
2025-10-17 14:54:35 +0300

    Refactor WebSocket service to improve session management and introduce utility methods for user and room handling

--- backend/websocket-hub-service/src/main/java/com/websocket_hub/manager/RoomManager.java
+++ backend/websocket-hub-service/src/main/java/com/websocket_hub/manager/RoomManager.java
@@ -1,8 +1,11 @@
 package com.websocket_hub.manager;
 
-import com.websocket_hub.entity.ClientSession;
-import com.websocket_hub.entity.Room;
+import com.websocket_hub.domain.entity.ClientSession;
+import com.websocket_hub.domain.entity.Room;
+import com.websocket_hub.enums.MessageType;
+import com.websocket_hub.enums.SystemEvents;
 import com.websocket_hub.factory.ObjectFactory;
+import com.websocket_hub.mapper.MessageMapper;
 import com.websocket_hub.serializer.MessageSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,8 +15,11 @@ import org.springframework.web.socket.WebSocketSession;
 @Slf4j
 public class RoomManager extends AbstractRoomManager {
 
-    public RoomManager(MessageSerializer<String> serializer, ObjectFactory<Room> roomFactory, ObjectFactory<ClientSession> clientFactory) {
+    private final MessageMapper mapper;
+
+    public RoomManager(MessageSerializer<String> serializer, ObjectFactory<Room> roomFactory, ObjectFactory<ClientSession> clientFactory, MessageMapper mapper) {
         super(serializer, roomFactory, clientFactory);
+        this.mapper = mapper;
     }
 
     @Override
@@ -22,12 +28,12 @@ public class RoomManager extends AbstractRoomManager {
     }
 
     @Override
-    protected void onAddSession(String roomName, WebSocketSession session) {
-
+    protected void onAddSession(String username, String roomName, WebSocketSession session) {
+        broadcast(roomName, mapper.toResponse(MessageType.SYSTEM, roomName, username + " " + SystemEvents.JOIN.getDescription() + " room: [" + roomName + "]"));
     }
 
     @Override
-    protected void onRemoveSession(String roomName, WebSocketSession session) {
-
+    protected void onRemoveSession(String username, String roomName, WebSocketSession session) {
+        broadcast(roomName, mapper.toResponse(MessageType.SYSTEM, roomName, username + " " + SystemEvents.LEFT.getDescription() + "room: [" + roomName + "]"));
     }
 }
