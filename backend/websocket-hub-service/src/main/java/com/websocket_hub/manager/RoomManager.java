caa89d9d1c4e4785cf492bc390d81e1827f3907e
Pavel Stankevich <pavelstankevich04@gmail.com>
2025-11-23 20:00:07 +0300

    And new parameter into constructor

--- backend/websocket-hub-service/src/main/java/com/websocket_hub/manager/RoomManager.java
+++ backend/websocket-hub-service/src/main/java/com/websocket_hub/manager/RoomManager.java
@@ -3,10 +3,12 @@ package com.websocket_hub.manager;
 import com.websocket_hub.domain.dto.user_service.UserInfoInternalResponse;
 import com.websocket_hub.domain.entity.Room;
 import com.websocket_hub.domain.enums.MessageType;
+import com.websocket_hub.domain.enums.RoomType;
 import com.websocket_hub.domain.enums.SystemEvent;
 import com.websocket_hub.factory.ObjectFactory;
 import com.websocket_hub.mapper.MessageMapper;
 import com.websocket_hub.serializer.MessageSerializer;
+import com.websocket_hub.service.RoomManagerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -22,9 +24,10 @@ public class RoomManager extends AbstractRoomManager {
             MessageSerializer<String> serializer,
             ObjectFactory<Room> roomFactory,
             SessionManager sessionManager,
+            RoomManagerService service,
             @Qualifier("messageMapperImpl") MessageMapper mapper
     ) {
-        super(serializer, roomFactory, sessionManager);
+        super(serializer, roomFactory, sessionManager, service);
         this.mapper = mapper;
     }
 
@@ -41,5 +44,10 @@ public class RoomManager extends AbstractRoomManager {
     @Override
     protected void onRemoveSession(UserInfoInternalResponse user, String roomName, WebSocketSession session) {
         broadcast(roomName, mapper.toResponse(MessageType.SYSTEM, SystemEvent.LEAVE, roomName, user.username() + " " + SystemEvent.LEAVE.getDescription() + "room: " + roomName));
+    }
+
+    @Override
+    protected boolean validateManagerType(RoomType roomType) {
+        return this.getClass().equals(roomType.getManagerClass());
     }
 }
