79a0dfaa141589b7c9b50ff25817f01db03926d6
Pavel Stankevich <pavelstankevich04@gmail.com>
2025-12-14 00:17:06 +0300

    Change transaction mapping

--- backend/bank-service/src/main/java/com/bank_service/mapper/TransactionMapper.java
+++ backend/bank-service/src/main/java/com/bank_service/mapper/TransactionMapper.java
@@ -1,7 +1,7 @@
 package com.bank_service.mapper;
 
+import com.bank_service.domain.dto.user_service.TransactionShortInfoRequest;
 import com.bank_service.domain.entity.Transaction;
-import com.bank_service.domain.entity.TransactionShortInfo;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -9,7 +9,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
 
-    TransactionShortInfo toShortInfo(Transaction transaction);
+    TransactionShortInfoRequest toShortInfo(Transaction transaction);
 
-    List<TransactionShortInfo> toShortInfoList(List<Transaction> transaction);
+    List<TransactionShortInfoRequest> toShortInfoList(List<Transaction> transaction);
 }
