349f8a7268a30e09640cd655513c7868bdb895a6
Timofei123456 <167242209+Timofei123456@users.noreply.github.com>
2026-03-02 10:41:02 +0300

    Merge implement-decode-game into dev (#68)

--- backend/bank-service/src/main/java/com/bank_service/domain/dto/GameTransactionRequest.java
+++ backend/bank-service/src/main/java/com/bank_service/domain/dto/GameTransactionRequest.java
@@ -10,12 +10,14 @@ import java.util.UUID;
 @JsonSubTypes({
         @JsonSubTypes.Type(value = TestRoomTransactionRequest.class, name = "ROOM_TEST"),
         @JsonSubTypes.Type(value = TicTacToeTransactionRequest.class, name = "TIC_TAC_TOE"),
-        @JsonSubTypes.Type(value = HorseRaceTransactionRequest.class, name = "HORSE_RACE")
+        @JsonSubTypes.Type(value = HorseRaceTransactionRequest.class, name = "HORSE_RACE"),
+        @JsonSubTypes.Type(value = DeCoderTransactionRequest.class, name = "DE_CODER"),
 })
 public sealed interface GameTransactionRequest permits
         TestRoomTransactionRequest,
         TicTacToeTransactionRequest,
-        HorseRaceTransactionRequest {
+        HorseRaceTransactionRequest,
+        DeCoderTransactionRequest {
 
     UUID roomId();
 
