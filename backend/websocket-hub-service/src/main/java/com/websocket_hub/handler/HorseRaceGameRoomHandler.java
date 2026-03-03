e8fda5d3923ed0746c3d18a90b877e26b2f26a10
Pavel <pavelstankevich04@gmail.com>
2026-03-03 17:25:02 +0300

    Hotfix timer-for-horse-race into dev (#69)

--- backend/websocket-hub-service/src/main/java/com/websocket_hub/handler/HorseRaceGameRoomHandler.java
+++ backend/websocket-hub-service/src/main/java/com/websocket_hub/handler/HorseRaceGameRoomHandler.java
@@ -13,6 +13,7 @@ import com.websocket_hub.domain.entity.HorseRacePlayerBet;
 import com.websocket_hub.domain.enums.MessageType;
 import com.websocket_hub.domain.enums.RoomStatus;
 import com.websocket_hub.domain.enums.events.HorseRaceEvent;
+import com.websocket_hub.event.CountdownExpiredEvent;
 import com.websocket_hub.manager.HorseRaceGameRoomManager;
 import com.websocket_hub.manager.SessionManager;
 import com.websocket_hub.mapper.DefaultMessageMapper;
