38fb074358910d4c69cb29372d877e2ed9a3f29f
Pavel Stankevich <pavelstankevich04@gmail.com>
2026-04-17 15:41:53 +0300

    implement transactional outbox JPA and message scheduling

--- backend/common-utils/kafka-starter/src/main/java/com/kafka_starter/config/KafkaTransactionalOutboxConfiguration.java
+++ backend/common-utils/kafka-starter/src/main/java/com/kafka_starter/config/KafkaTransactionalOutboxConfiguration.java
@@ -1,18 +1,28 @@
 package com.kafka_starter.config;
 
+import com.kafka_starter.service.KafkaTransactionalOutboxMessageScheduler;
+import com.kafka_starter.service.KafkaTransactionalOutboxMessageService;
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
 @ConditionalOnProperty(prefix = "kafka.transactional-outbox", name = "enabled", havingValue = "true")
 @ConditionalOnClass(SpringLiquibase.class)
+@EnableJpaRepositories(basePackages = "com.kafka_starter")
+@AutoConfigurationPackage(basePackages = "com.kafka_starter")
 @EnableConfigurationProperties(KafkaTransactionalOutboxProperties.class)
-@Import(KafkaStarterLiquibaseConfig.class)
+@Import({
+        KafkaStarterLiquibaseConfig.class,
+        KafkaTransactionalOutboxMessageService.class,
+        KafkaTransactionalOutboxMessageScheduler.class
+})
 public class KafkaTransactionalOutboxConfiguration {
 }
