0181bfe46c9e77760f71cbdfddd0147e8af245c9
Pavel Stankevich <pavelstankevich04@gmail.com>
2026-05-28 19:07:55 +0300

    update swagger configuration

--- backend/api-gateway/src/main/java/casualgames/apigateway/config/SwaggerConfig.java
+++ backend/api-gateway/src/main/java/casualgames/apigateway/config/SwaggerConfig.java
@@ -1,19 +1,29 @@
 package casualgames.apigateway.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import java.net.URI;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Api gateway API", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
-    @Bean
+    @Value("${server-url}")
+    private String url;
+
+    /*@Bean
     public RouterFunction<ServerResponse> swaggerUiRedirect() {
         return RouterFunctions.route(
                 RequestPredicates.GET("/swagger-ui.html"),
@@ -22,5 +32,11 @@ public class SwaggerConfig {
                         .location(URI.create("/webjars/swagger-ui/index.html"))
                         .build()
         );
+    }*/
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url(url));
     }
 }
