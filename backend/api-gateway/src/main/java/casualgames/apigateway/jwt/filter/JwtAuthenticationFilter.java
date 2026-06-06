00affea77bc5aaa1cc4ebe2453baa1758a7f63cf
Pavel <pavelstankevich04@gmail.com>
2026-06-06 17:27:40 +0300

    Merge update-authentication-system into dev (#149)

--- backend/api-gateway/src/main/java/casualgames/apigateway/jwt/filter/JwtAuthenticationFilter.java
+++ backend/api-gateway/src/main/java/casualgames/apigateway/jwt/filter/JwtAuthenticationFilter.java
@@ -168,9 +168,4 @@ public class JwtAuthenticationFilter implements WebFilter, Ordered {
     public int getOrder() {
         return 0;
     }
-
-    /*@Override
-    public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
-    }*/
 }
