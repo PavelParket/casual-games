2c1ab6fe090f6d0119cef58b0e28a649873f45b3
Pavel <pavelstankevich04@gmail.com>
2026-07-03 18:59:43 +0300

    Revert "#noissue update CI workflows and add CodeQL analysis (#181)"
    This reverts commit 1e322bd463ef171a2868fbf852775fe13f9093d1.

--- backend/game-service/src/main/java/com/game_service/de_coder/mapper/DeCoderGameMapper.java
+++ backend/game-service/src/main/java/com/game_service/de_coder/mapper/DeCoderGameMapper.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 @Mapper(componentModel = "spring")
 public interface DeCoderGameMapper {
 
-    @Mapping(target = "player", ignore = true)
     @Mapping(target = "winner", ignore = true)
     @Mapping(target = "gameState", ignore = true)
     @Mapping(target = "isGameStarted", ignore = true)
